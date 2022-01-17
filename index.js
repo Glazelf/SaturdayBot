18170ad4bb4c630d0f7a5abe0e80ad5da32e2382
Glazelf <gideon.steenbergen@gmail.com>
2022-01-17 03:17:45 +0100

    import code from tweeterbot

---
+++ index.js
@@ -0,0 +1,23 @@
+const readline = require('readline-sync');
+const config = require('./config.json');
+const Twitter = require('twitter-lite');
+const client = new Twitter(config);
+
+// Adaptive tweet text and checks
+let tweetText = readline.question("Tweet body: ");
+if (tweetText.length > 280) {
+    return console.log("Tweet body can only be 280 characters long.");
+} else if (tweetText.length < 1) {
+    return console.log("Tweet body has to be at least 1 character long.");
+};
+
+// Set body to an object
+let postBody = {
+    'status': tweetText
+};
+
+// Tweet
+client.post('statuses/update', postBody).catch(console.error).then(result => {
+    // console.log(result);
+    return console.log(`You successfully tweeted: "${result.text}"`);
+});
