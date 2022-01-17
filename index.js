a2e09c3517953b57ce9ca3dc5ec120a88ecbb92f
Glazelf <gideon.steenbergen@gmail.com>
2022-01-17 04:01:57 +0100

    this should work
    me when auth issues

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-const readline = require('readline-sync');
+const cron = require("cron");
+const timezone = 'cest';
+// const saturdayTime = '00 00 18 * * 6'; // 6pm every saturday
+const saturdayTime = '00 2 4 * * *'; // testing time
+const fs = require("fs");
 const config = require('./config.json');
 const Twitter = require('twitter-lite');
 const client = new Twitter(config);
 
-// Adaptive tweet text and checks
-let tweetText = readline.question("Tweet body: ");
-if (tweetText.length > 280) {
-    return console.log("Tweet body can only be 280 characters long.");
-} else if (tweetText.length < 1) {
-    return console.log("Tweet body has to be at least 1 character long.");
-};
+let filePath = './assets/saturdayimage.png';
+let saturdayimg = fs.readFileSync(filePath);
 
-// Set body to an object
-let postBody = {
-    'status': tweetText
-};
+console.log("Awaiting cronjob...");
 
-// Tweet
-client.post('statuses/update', postBody).catch(console.error).then(result => {
-    // console.log(result);
-    return console.log(`You successfully tweeted: "${result.text}"`);
-});
+// Send Nintendo direct prediction
+new cron.CronJob(saturdayTime, async () => {
+    tweet(saturdayimg);
+}, timeZone = timezone, start = true);
+
+// Tweet function
+function tweet(saturdayimg) {
+
+    // Set body to an object
+    let postBody = {
+        'media_data': saturdayimg
+    };
+
+    // Tweet
+    client.post('statuses/update', postBody).catch(console.error).then(result => {
+        console.log(result);
+    });
+};
